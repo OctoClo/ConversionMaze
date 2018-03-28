bf3e73809914a17fb32f141895262aa282826db2
OctoClo <octoclo@zoho.com>
2018-03-28 18:49:53 +0200

    Working HUD : HealthBar, Minimap & Timer

---
+++ ConversionFPS/src/HUD/Timer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ConversionFPS
+{
+    class Timer
+    {
+        float timeF;
+        int time, minutes, seconds;
+        string timer, level;
+
+        Vector2 timePosition, levelPosition;
+
+        public Timer()
+        {
+            timeF = 180f;
+            timePosition = new Vector2((Main.Width / 2) - (HUD.Font.MeasureString("2:22").X / 2), 50);
+            levelPosition = new Vector2((Main.Width / 2) - (HUD.FontTiny.MeasureString("level 2").X / 2), 15);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            timeF -= gameTime.ElapsedGameTime.Milliseconds / 1000f;
+            if (timeF < 0)
+                Main.GameState = GameState.GameOver;
+
+            time = MathHelper.Clamp((int)(Math.Round(timeF) + .5f), 0, 999);
+            minutes = time / 60;
+            seconds = time % 60;
+
+            level = "level " + Main.LevelNumber;
+
+            if (seconds < 10)
+                timer = minutes + ":0" + seconds;
+            else
+                timer = minutes + ":" + seconds;
+        }
+
+        public void Draw()
+        {
+            Main.Batch.DrawString(HUD.FontTiny, level, levelPosition, Color.White);
+            if ( time >= 10 )
+                Main.Batch.DrawString(HUD.Font, timer, timePosition, Color.White);
+            else
+                Main.Batch.DrawString(HUD.Font, timer, timePosition, Color.DarkRed);
+        }
+    }
+}
--- ConversionFPS/src/Main.cs
+++ ConversionFPS/src/Main.cs
@@ -1,44 +1,115 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 
 namespace ConversionFPS
 {
+    enum GameState { Playing, Win, GameOver };
+
     class Main
     {
         public static Game1 Instance;
         public static GraphicsDeviceManager Graphics;
+        public static GraphicsDevice Device;
+        public static SpriteBatch Batch;
         public static Random Rand;
 
-        public static int Width, Height;
+        public static int Height, Width;
+
+        public static int LevelNumber;
+        public static Vector3 PlayerPosition;
+        public static float Rotation;
+
+        public static GameState GameState;
+
+        Vector2 center;
+        float speed, rotationSpeed;
+        HUD hud;
 
         public Main(Game1 game, GraphicsDeviceManager graphics)
         {
             Instance = game;
             Graphics = graphics;
+            Device = Graphics.GraphicsDevice;
+            Batch = new SpriteBatch(Device);
             Rand = new Random();
 
-            Width = 800;
-            Height = 600;
+            Height = 800;
+            Width = 1280;
+            center = new Vector2(Width / 2, Height / 2);
+
+            LevelNumber = 1;
+            PlayerPosition = Vector3.Zero;
+            Rotation = 0;
+
+            GameState = GameState.Playing;
+
+            speed = 2f;
+            rotationSpeed = 3f;
+            hud = new HUD();
         }
 
         public void Initialize()
         {
-            Graphics.PreferredBackBufferWidth = Width;
             Graphics.PreferredBackBufferHeight = Height;
+            Graphics.PreferredBackBufferWidth = Width;
             Graphics.SynchronizeWithVerticalRetrace = false;
             Graphics.ApplyChanges();
+
+            Instance.Window.Position = new Point((GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2) - (Width / 2),
+                                                 (GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2) - (Height / 2) - 40);
         }
 
-        public void Update(GameTime time)
+        public void Update(GameTime gameTime)
         {
+            if (GameState != GameState.Playing)
+                Instance.Exit();
+
             Input.Update();
+            hud.Update(gameTime);
+
+            //Lock or unlock mouse movement
+            if (Instance.IsActive)
+            {
+                if (Input.MousePos.X != center.X)
+                    Rotation += (Input.MousePos.X - center.X) / rotationSpeed;
+
+                Mouse.SetPosition((int)center.X, (int)center.Y);
+                Instance.IsMouseVisible = false;
+            }
+
+            if (Input.KeyPressed(Keys.Z, false))
+            {
+                PlayerPosition.X += speed * (float)Math.Sin((double)MathHelper.ToRadians(Rotation));
+                PlayerPosition.Y -= speed * (float)Math.Cos((double)MathHelper.ToRadians(Rotation));
+            }
+            if (Input.KeyPressed(Keys.S, false))
+            {
+                PlayerPosition.X += speed * (float)Math.Sin((double)MathHelper.ToRadians(Rotation + 180));
+                PlayerPosition.Y -= speed * (float)Math.Cos((double)MathHelper.ToRadians(Rotation + 180));
+            }
+            if (Input.KeyPressed(Keys.Q, false))
+            {
+                PlayerPosition.X += speed * (float)Math.Sin((double)MathHelper.ToRadians(Rotation - 90));
+                PlayerPosition.Y -= speed * (float)Math.Cos((double)MathHelper.ToRadians(Rotation - 90));
+            }
+            if (Input.KeyPressed(Keys.D, false))
+            {
+                PlayerPosition.X += speed * (float)Math.Sin((double)MathHelper.ToRadians(Rotation + 90));
+                PlayerPosition.Y -= speed * (float)Math.Cos((double)MathHelper.ToRadians(Rotation + 90));
+            }
+
+            PlayerPosition.X = MathHelper.Clamp(PlayerPosition.X, 0f, 780f);
+            PlayerPosition.Y = MathHelper.Clamp(PlayerPosition.Y, 0f, 580f);
         }
 
         public void Draw()
         {
-            Graphics.GraphicsDevice.Clear(Color.Black);
+            Device.Clear(Color.Black);
+            Batch.Begin();
+            hud.Draw();
+            Batch.End();
         }
     }
 }
